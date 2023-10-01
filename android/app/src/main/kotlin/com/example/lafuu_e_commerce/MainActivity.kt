4c704973d77869f7c94f6b9ea14b5e9dcf5bb6de
KerolosCS <kerolosfady4@gmail.com>
2023-10-01 21:31:51 +0300

    solve_error_in_C

--- android/app/src/main/kotlin/com/example/lafuu_e_commerce/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/lafuu_e_commerce/MainActivity.kt
@@ -1,15 +1,3 @@
-/*
- *
- * ----------------
- * | 241030072002 |
- * ----------------
- * Copyright © [2023] KERO CS FLUTTER DEVELOPMENT.
- * All Rights Reserved. For inquiries or permissions, contact  me ,
- * https://www.linkedin.com/in/kerolos-fady-software-engineer/
- *
- * /
- */
-
 package com.example.lafuu_e_commerce
 
 import io.flutter.embedding.android.FlutterActivity
